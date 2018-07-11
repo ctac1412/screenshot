867724faff41afd709945965576efe031a8c76d2
daniil <hedgehog147@gmail.com>
2018-07-11 15:10:39 +0300

    добавленно: динамическое создание каталогов и инициализация рабочей области(из бд)

--- screen.py
+++ screen.py
@@ -4,45 +4,18 @@ import datetime
 import os
 import math
 import postgresql
-# db = postgresql.open('pq://postgres:postgres@localhost:5432/postgres')
-# db.execute("CREATE TABLE screenshots (id SERIAL PRIMARY KEY, "
-#                "image_path CHAR(64), screen_area CHAR(2), created_at TIMESTAMP DEFAULT NOW())")
+
 def start():
+    checkIsFolderExist()
     folder_name = datetime.datetime.now().date()
-    if not os.path.exists(str(folder_name)):
-        os.makedirs(str(folder_name))
-
-    if not os.path.exists(str(folder_name) + "/1"):
-        os.makedirs(str(folder_name) + "/1")
-
-    if not os.path.exists(str(folder_name) + "/2"):
-        os.makedirs(str(folder_name) + "/2")
-
-    if not os.path.exists(str(folder_name) + "/3"):
-        os.makedirs(str(folder_name) + "/3")
-
-    if not os.path.exists(str(folder_name) + "/4"):
-        os.makedirs(str(folder_name) + "/4")
-
     for iteration in range(3):
-        img_name = math.floor(time.time())
         time.sleep(1)
-        
-        img1 = ImageGrab.grab(bbox=(0, 0, 300, 300))
-        img1.save(str(folder_name) + "/1/" + str(img_name) + ".png", "PNG")
-        insertImagePathIntoDb(str(folder_name) + "/1/" + str(img_name) + ".png","1")
-
-        img2 = ImageGrab.grab(bbox=(300, 0, 600, 300))
-        img2.save(str(folder_name) + "/2/" + str(img_name) + ".png", "PNG")
-        insertImagePathIntoDb(str(folder_name) + "/2/" + str(img_name) + ".png","2")
-
-        img3 = ImageGrab.grab(bbox=(0, 300, 300, 600))
-        img3.save(str(folder_name) + "/3/" + str(img_name) + ".png", "PNG")
-        insertImagePathIntoDb(str(folder_name) + "/3/" + str(img_name) + ".png","3")
-
-        img4 = ImageGrab.grab(bbox=(300, 300, 600, 600))
-        img4.save(str(folder_name) + "/4/" + str(img_name) + ".png", "PNG")
-        insertImagePathIntoDb(str(folder_name) + "/4/" + str(img_name) + ".png","4")
+        for item in getScreenData():
+            image_name = math.floor(time.time())
+            image_path = str(folder_name) + "/" + str(item['screen_area']) + "/" + str(image_name) + ".png"
+            image = ImageGrab.grab(bbox=(item['x_coordinate'], item['y_coordinate'], item['width'], item['height']))
+            image.save(image_path, "PNG")
+            insertImagePathIntoDb(image_path,str(item['screen_area']))
 
 
 def insertImagePathIntoDb(image_path,screen_area):
@@ -50,5 +23,18 @@ def insertImagePathIntoDb(image_path,screen_area):
     insert = db.prepare("INSERT INTO screenshots (image_path,screen_area) VALUES ($1,$2)")
     insert(image_path,screen_area)
 
+def getScreenData():
+    db = postgresql.open('pq://postgres:postgres@localhost:5432/postgres')
+    data = db.query("select x_coordinate,y_coordinate,width,height,screen_area from screen_coordinates")
+    return data
 
+def checkIsFolderExist():
+    folder_name = datetime.datetime.now().date()
+    if not os.path.exists(str(folder_name)):
+        os.makedirs(str(folder_name))
+    db = postgresql.open('pq://postgres:postgres@localhost:5432/postgres')
+    data = db.query("select screen_area from screen_coordinates")
+    for value in data:
+        if not os.path.exists(str(folder_name) + "/" + str(value['screen_area'])):
+            os.makedirs(str(folder_name) + "/" + str(value['screen_area']))
 
