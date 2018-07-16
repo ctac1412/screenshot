2dbb151842b9787ed42883129be1d51732864c02
daniil <hedgehog147@gmail.com>
2018-07-16 17:22:57 +0300

    fix

--- screen.py
+++ screen.py
@@ -3,7 +3,7 @@ import time
 import datetime
 import math
 import image_processing
-import postgresql
+import session_log
 
 images_folder = "images/"
 
@@ -11,13 +11,19 @@ def start():
     folder_name = images_folder + str(datetime.datetime.now().date())
     time.sleep(2)
     for item in image_processing.getScreenData():
-        #Если последняя строка для текущей области имеет статус отличный от null
+        image_name = str(math.floor(time.time()))
+        image_path = folder_name + "/" + str(item['screen_area']) + "/" + image_name + ".png"
+        # Делаем скрин указанной области экрана
+        image = ImageGrab.grab(bbox=(item['x_coordinate'], item['y_coordinate'], item['width'], item['height']))
+        # Сохраняем изображение на жестком диске
+        image.save(image_path, "PNG")
+        # Сохраняем инфо в бд
+        image_processing.insertImagePathIntoDb(image_path, str(item['screen_area']))
+
+        # Если последняя строка для текущей области имеет статус отличный от null
         if image_processing.getLastScreen(str(item['screen_area']) is not None):
-            image_name = str(math.floor(time.time()))
-            image_path = folder_name + "/" + str(item['screen_area']) + "/" + image_name + ".png"
-            # Делаем скрин указанной области экрана
-            image = ImageGrab.grab(bbox=(item['x_coordinate'], item['y_coordinate'], item['width'], item['height']))
-            # Сохраняем изображение на жестком диске
-            image.save(image_path, "PNG")
-            # Сохраняем инфо в бд
-            image_processing.insertImagePathIntoDb(image_path, str(item['screen_area']))
+            hand = image_processing.searchPlayerHand(str(item['screen_area']))
+            #Если рука обнаружена на скрине
+            if hand != '':
+                session_log.insertIntoLogSession(str(item['screen_area']), hand)
+
