c7008ca3156ae4163fc9b8cf50163e1ffdb5d721
daniil <hedgehog147@gmail.com>
2018-07-19 09:12:41 +0300

    fix

--- test.py
+++ test.py
@@ -4,6 +4,14 @@
 # import image_processing
 import random
 
+message = 'TsTd'
+val = ''
+for c in message:
+    if c.isupper():
+        val += c
+if val[0] == val[1]:
+    print('pair')
+else: print('none')
 # def errorLog(module_name,error_message):
 #     db = postgresql.open('pq://postgres:postgres@localhost:5433/postgres')
 #     insert = db.prepare("insert into error_log (module_name,error_message) values($1,$2)")
