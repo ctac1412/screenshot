adde8366b605ded6a9cd524e11f0dee794b9a8db
daniil <hedgehog147@gmail.com>
2018-07-20 09:05:14 +0300

    fix

--- keyboard.py
+++ keyboard.py
@@ -1,10 +1,14 @@
-import pyautogui
+# import pyautogui
+# #
+# # def push():
+# #     pyautogui.press('q')
+# #
+# # def check():
+# #     pyautogui.press('c')
+# #
+# # def checkFold():
+# #     pyautogui.press('f')
 
-def push():
-    pyautogui.press('q')
+import test
 
-def check():
-    pyautogui.press('c')
-
-def checkFold():
-    pyautogui.press('f')
+print(test.getDecision('KsKd'))
--- test.py
+++ test.py
@@ -4,14 +4,47 @@
 # import image_processing
 import random
 
-message = 'TsTd'
-val = ''
-for c in message:
-    if c.isupper():
-        val += c
-if val[0] == val[1]:
-    print('pair')
-else: print('none')
+
+
+def getDecision(hand):
+    if pocketBroadway(hand) == 1 or pocketPair(hand) == 1 or anyAce(hand) == 1:
+        return 1
+    else:
+        return 0
+
+
+def pocketBroadway(hand):
+    val = ''
+    broadway = ['A', 'K', 'Q', 'J', 'T']
+    for value in hand:
+        if value.isupper():
+            val += value
+    try:
+        if val[0] in broadway and val[1] in broadway:
+            return 1
+        else:
+            return 0
+    except:
+        return 0
+def pocketPair(hand):
+    val = ''
+    for c in hand:
+        if c.isupper() or c.isdigit():
+            val += c
+    if val[0] == val[1]:
+        return 1
+    else:
+        return 0
+
+def anyAce(hand):
+    val = ''
+    for c in hand:
+        if c.isupper() or c.isdigit():
+            val += c
+    if val[0] == 'A' or val[1] == 'A':
+        return 1
+    else:
+        return 0
 # def errorLog(module_name,error_message):
 #     db = postgresql.open('pq://postgres:postgres@localhost:5433/postgres')
 #     insert = db.prepare("insert into error_log (module_name,error_message) values($1,$2)")
