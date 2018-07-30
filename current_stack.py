35d6a6e37e3abf2abb05f2937bdde5b3bbfdd8dc
daniil <hedgehog147@gmail.com>
2018-07-30 17:15:02 +0300

    fix

--- current_stack.py
+++ current_stack.py
@@ -1,1 +1,12 @@
-    
+import db_conf
+import postgresql
+
+
+def searchCurrentStack(screen_area):
+    return 0
+
+#Получаем номер области экрана, на которой нужно искать элемент для текущего стола
+def getStackArea(screen_area):
+    db = postgresql.open(db_conf.connectionString())
+    data = db.query("select stack_area from screen_coordinates where screen_area = " + screen_area)
+    return data[0]['stack_area']
