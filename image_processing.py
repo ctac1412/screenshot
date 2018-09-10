ff0d16ae693892514fe8afe68a759b0c5e2d5438
Daniil Arashkevich <cruim@MacBook-Pro-Daniil.local>
2018-09-10 10:23:18 +0300

    fix

--- image_processing.py
+++ image_processing.py
@@ -72,9 +72,9 @@ def getFlopCards():
     return data
 
 #Получение последнего скрина для текущей области экрана
-def getLastScreen(screen_area):
+def getLastScreen(screen_area, limit=1):
     db = postgresql.open(db_conf.connectionString())
-    data = db.query("select trim(image_path)as image_path from screenshots where screen_area = " + str(screen_area) + " order by id desc limit 1")
+    data = db.query("select trim(image_path)as image_path from screenshots where screen_area = " + str(screen_area) + " order by id desc limit " + limit)
     return data
 
 #Получение инфа для поиска элемента на изображении
