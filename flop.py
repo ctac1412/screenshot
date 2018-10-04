2c5106bdb8a3dcbab3537d4d6798aef2350fe48a
Daniil Arashkevich <cruim@MacBook-Pro-Daniil.local>
2018-10-04 10:24:06 +0300

    fix

--- flop.py
+++ flop.py
@@ -73,11 +73,13 @@ def checkPair(hand):
     for item in hand:
         counter[item] = counter.get(item, 0) + 1
     doubles = {element: count for element, count in counter.items() if count > 1}
-    if len(doubles) > 0:
+    if len(doubles) == 1:
         double_element = list(doubles.keys())[0]
         if double_element in [hand[0], hand[1]] and ranks.index(double_element) != min(ts) or \
                 list(doubles.values())[0] > 2 and double_element in [hand[0], hand[1]]:
             return True
+    elif len(doubles) == 2:
+        return True
     return False
 
 #Получаем номер области экрана, на которой нужно искать элемент для текущего стола
--- test.py
+++ test.py
@@ -1,15 +1,6 @@
 import flop
 import session_log
 import image_processing
-# hand = '5h4h4c6h7s'
-# # print(flop.checkPair('3h6s3c3h3s'))
-#
-# if flop.checkPair(hand) or flop.checkStraightDraw(hand) or flop.checkFlushDraw(hand):
-#     print(1)
-
-# row = session_log.getLastRowFromLogSession('3')
-last_row = session_log.getLastRowFromLogSession('3')
-hand = last_row[0][0]
-stack = last_row[0][1]
-action = last_row[0][3]
-print(action)
+hand = '5hKh8c5hKs'
+# hand = '5hKh8c5h8s'
+print(flop.checkPair(hand))
