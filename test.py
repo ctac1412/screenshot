c30c6d26b165bf7ab312f9b99fbb3cec1f75d17e
danya <you@example.com>
2018-10-05 10:08:23 +0300

    fix

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import flop
 import session_log
 import image_processing
+import error_log
 hand = '5hKh8c5hKs'
 # hand = '5hKh8c5h8s'
-print(flop.checkPair(hand))
+error_log.errorLog('test','teggxxxxst')
