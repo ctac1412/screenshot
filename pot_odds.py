1c9f876aa741e3275a504bc58aa421c58176f649
danya <you@example.com>
2018-12-25 11:00:30 +0300

    fix

--- pot_odds.py
+++ pot_odds.py
@@ -7,14 +7,14 @@ def check_is_call_valid(screen_area, hand_value, element, stack_collection, db):
     cur_stack = current_stack.search_current_stack(screen_area, stack_collection, db)
     bank_size = current_stack.search_bank_stack(screen_area, db)
     call_size = image_processing.search_last_opponent_action(screen_area, db)
-    if call_size == '0.5':
-        call_size = float(call_size)
-    else:
-        call_size = int(call_size)
     if not isinstance(call_size, str):
         call_size = call_size['alias']
     else:
         return False
+    if call_size == '0.5':
+        call_size = float(call_size)
+    else:
+        call_size = int(call_size)
     current_pot_odds = round(bank_size / call_size, 1)
     if cur_stack <= call_size:
         element = 'river'
